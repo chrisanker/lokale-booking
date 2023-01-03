bd65cfa3ae6f4f54b94f29111c996b1aec93d80f
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2023-01-03 22:30:18 +0100

    added grid styling

--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,23 @@ import './Search.css'
 
 export function Search() {
     return (
-        <div className="grid-container">
-            <p>
+        <div>
+            <h1>
                 Hvad vil du booke?
-            </p>
-            <div>
-                <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/>
-                <img src="https://thumbs.dreamstime.com/b/aaiae-137315496.jpg" alt="Meeting room equipment icon"/>
+            </h1>
+            <div className="grid-container">
+                <div className="grid-item-1">
+                    <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/>
+                </div>
+                <div className="grid-item-2">
+                    <img src="https://thumbs.dreamstime.com/b/aaiae-137315496.jpg" alt="Meeting room equipment icon"/>
+                </div>
+                <div className="grid-item-3">
+                    Mødelokale
+                </div>
+                <div className="grid-item-4">
+                    Udstyr til mødelokale
+                </div>
             </div>
         </div>
     )
