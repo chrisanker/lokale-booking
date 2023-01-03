5a45a443377db56c8d1a8644e11a7e2b4569dc9d
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2023-01-03 14:20:51 +0100

    Search component initial commit

---
+++ src/Search.js
@@ -0,0 +1,13 @@
+export function Search() {
+    return (
+        <>
+            <p>
+                Hvad vil du booke?
+            </p>
+            <div>
+                <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/>
+                <img src=""/>
+            </div>
+        </>
+    )
+}
