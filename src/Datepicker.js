bef9c0fc69651303458163b6374d5814be30ad87
Christian Anker Larsen <calanker@gmail.com>
2023-01-06 14:12:16 +0100

    Refactoring

--- src/Datepicker.js
+++ src/Datepicker.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-
 import "react-datepicker/dist/react-datepicker.css";
 
 export function SelectDate(props) {
     const [startDate, setStartDate] = useState(new Date());
-  return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-  );
+
+    return (
+        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    );
 }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -9,7 +9,12 @@ export class SearchPage extends React.Component{
     }
 
     handleUpdate = () => {
-        this.setState({isDatePickerVisible: true})
+        if (this.state.isDatePickerVisible) {
+            this.setState({isDatePickerVisible: false})
+        }
+        else {
+            this.setState({isDatePickerVisible: true})
+        }
     }
     
     render() {
@@ -18,12 +23,12 @@ export class SearchPage extends React.Component{
                 <h1>
                     Hvad vil du booke?
                 </h1>
-                <div className="grid-container" onClick={this.handleUpdate}>
-                    <div className="grid-item-1">
+                <div className="grid-container">
+                    <div className="grid-item-1" onClick={this.handleUpdate}>
                         <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/><br/>
                         Mødelokale
                     </div>
-                    <div className="grid-item-2">
+                    <div className="grid-item-2" onClick={this.handleUpdate}>
                         <img src="https://thumbs.dreamstime.com/b/aaiae-137315496.jpg" alt="Meeting room equipment icon"/><br/>
                         Udstyr til mødelokale
                     </div>
