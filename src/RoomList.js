45ed2bf9d2b6d3b2acd3677cbf1234484229b5c8
Christian Anker Larsen <calanker@gmail.com>
2023-01-08 17:37:20 +0100

    refactoring

--- src/RoomList.js
+++ src/RoomList.js
@@ -3,7 +3,12 @@ import { Room } from "./Room";
 
 export class RoomList extends React.Component {
     render() {
-        const meetingRooms = ['Meeting Room 1','Meeting Room 2','Meeting Room 3','Meeting Room 4']
+        const meetingRooms = [
+            'Meeting Room 1',
+            'Meeting Room 2',
+            'Meeting Room 3',
+            'Meeting Room 4'
+        ];
         const final = [];
         for (let i = 0; i < meetingRooms.length; i++){
             final.push(<li><Room name={meetingRooms[i]} /></li>);
@@ -17,4 +22,4 @@ export class RoomList extends React.Component {
             </React.Fragment>
         )
     }
-    }
+}
