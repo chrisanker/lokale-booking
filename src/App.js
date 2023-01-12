c5d8056216d53e5edde1cd3812e140b09c1cb0e7
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2023-01-12 21:10:46 +0100

    Refactoring App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React, { useState } from 'react';
 import './css/App.css';
 import {Resource} from "./components/Resource";
 import {SelectDate} from "./components/Datepicker";
-import {RoomList} from "./components/RoomList";
-import {Booking} from "./components/Booking";
 function App() {
-    const [isRoomSelected, setIsRoomSelected] = useState(false);
-
     return (
         <React.Fragment>
-            {isRoomSelected ? <Booking /> :
-                <div>
-                    <Resource />
-                    <SelectDate/>
-                </div>
-            }
+            <div>
+                <Resource />
+                <SelectDate/>
+            </div>
         </React.Fragment>
     );
 }
