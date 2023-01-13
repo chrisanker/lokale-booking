b26c0361532d90cc83a314211f5da3d89c689983
Christian Anker Larsen <calanker@gmail.com>
2023-01-13 22:56:43 +0100

    added form onSubmit handler

--- src/components/BookingDetails.js
+++ src/components/BookingDetails.js
@@ -3,6 +3,12 @@ import React from "react";
 export function BookingDetails(props) {
     const selection = props.selection;
     const date = props.date;
+    
+    const handleSubmit = (e) => {
+        const email = e.target.email.value;
+        alert("Tak for din booking. En bekræftelse vil blive sendt til " + email);
+    }
+
     return (
         <React.Fragment>
             <h1>Færdiggør Booking</h1>
@@ -14,12 +20,12 @@ export function BookingDetails(props) {
                     <p><span>Dato:</span> {date}</p>
                 </div>
                 <div>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <h3>Dine Oplysninger</h3>
                         <label>Navn <span>*</span></label><br />
-                        <input type={"text"} required /><br />
+                        <input id={"name"} type={"text"} required /><br />
                         <label>Email <span>*</span></label><br />
-                        <input type={"email"}></input><br /><br />
+                        <input id={"email"} type={"email"} required /><br /><br />
                         <button type={"submit"}>Bekræft</button>
                     </form>
                 </div>
