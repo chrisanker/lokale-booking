7a17cbef05903bdd9483b3101fcabd154a83fefc
Christian Anker Larsen <calanker@gmail.com>
2023-01-13 21:11:49 +0100

    updated Booking component layout

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -3,15 +3,27 @@ import React from "react";
 export function Booking(props) {
     const selection = props.selection;
     const date = props.date;
-    return(
+    return (
         <React.Fragment>
-            <h1>Bookingdetaljer</h1>
-            <form>
-                <label>Du har valgt</label>
-                <input type={"text"} readOnly={true} value={selection} /><br/>
-                <label>Dato</label>
-                <input type={"date"} readOnly={true} value={date}></input>
-            </form>
+            <h1>Færdiggør Booking</h1>
+            <div className="booking-container ">
+            <div></div>
+                <div>
+                    <h3>Bookingdetaljer</h3>
+                    <p>{selection}</p>
+                    <p>{date}</p>
+                </div>
+                <div>
+                    <form>
+                        <h3>Dine Oplysninger</h3>
+                        <label>Navn <span>*</span></label><br />
+                        <input type={"text"} required /><br />
+                        <label>Email <span>*</span></label><br />
+                        <input type={"email"}></input><br />
+                        <button type={"submit"}>Bekræft</button>
+                    </form>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
