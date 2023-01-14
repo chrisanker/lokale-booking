adb11dbc5bfc140184806d1f35ed7c4d7646c4d0
Christian Anker Larsen <calanker@gmail.com>
2023-01-14 21:34:06 +0100

    styling date picker and removed dead code

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export function SelectDate(props) {
@@ -9,7 +8,7 @@ export function SelectDate(props) {
     return (
         <div>
             <h2>Hvornår skal du bruge det?</h2>
-            <div id="datepicker-container">
+            <div className="datepicker-container" id="datepicker-container">
                 <div>
                     <input type={"date"} value={date} onInput={(e) => setDate(e.target.value)}></input>
                 </div>
