bfcada9a2a70de111a428484cc3402fcd56e7709
Christian Anker Larsen <calanker@gmail.com>
2023-01-14 22:31:34 +0100

    added span element to Confirmation component

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -7,7 +7,7 @@ export function Confirmation(props) {
         <React.Fragment>
             <h1>Tak for din booking.</h1>
             <div className="confirmation-container">
-                <p>En bekræftelse er blevet sendt til {email}.</p>
+                <p>En bekræftelse er blevet sendt til <span>{email}</span>.</p>
                 <p>Ønsker du at booke et andet mødelokale?</p>
                 <div className="confirmation-buttons">
                     <button onClick={() => props.handleClick()}>Ja</button>
