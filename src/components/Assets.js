4b3f471f7144f2f0a13879f297c5de53cfc8d339
Christian Anker Larsen <calanker@gmail.com>
2023-01-15 20:44:37 +0100

    added p elements to Assets comp

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -8,10 +8,12 @@ export function Assets() {
                 <div>
                     <div className="img-container">
                         <div>
-                            <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/>    
+                            <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/>
+                            <p>Mødelokale</p>
                         </div>
                         <div>
-                            <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/>    
+                            <img src="https://static.thenounproject.com/png/1211373-200.png" alt="Meeting room icon"/>
+                            <p>Udstyr</p>
                         </div>                        
                     </div>
                 </div>
