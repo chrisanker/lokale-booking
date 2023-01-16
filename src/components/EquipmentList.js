1f084c3eb5dafd08589cf632ed19761f7eed6fdb
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2023-01-16 14:06:58 +0100

    Styling equipment list

--- src/components/EquipmentList.js
+++ src/components/EquipmentList.js
@@ -2,14 +2,19 @@ import React from "react";
 
 export function EquipmentList() {
     const assets = ["Conference Phone", "Projector", "Surface Hub", "Video Camera"];
-    const listAssets = assets.map((assets) =>
-        <li>{assets}</li>
+    const listAssets = assets.map((asset) =>
+        <div key={asset.toString()} className={"equipment-list-container"}>
+            <label>{asset}</label>
+            <input type={"checkbox"}/>
+        </div>
     );
 
     return (
         <React.Fragment>
             <h2>Ledigt Udstyr</h2>
-            <ul>{listAssets}</ul>
+            <div >
+                <ul >{listAssets}</ul>
+            </div>
         </React.Fragment>
     )
 }
