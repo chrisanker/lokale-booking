4777ce22245b4adaaa27e4661e3926b796172e4c
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2023-01-16 11:40:23 +0100

    added new library to set min value on date picker

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
 
 export function SelectDate(props) {
 
     const [date, setDate] = useState(props?.value ?? '');
+    let today = moment().format("YYYY-MM-DD");
 
     return (
         <div>
             <h2>Hvornår skal du bruge det?</h2>
             <div className="datepicker-container">
                 <div>
-                    <input type={"date"} value={date} onInput={(e) => setDate(e.target.value)}></input><br /><br />
+                    <input type={"date"} min={today} value={date} onInput={(e) => setDate(e.target.value)}></input><br /><br />
                     <div id="datepicker-equipment-select">
                         <input type={"checkbox"} onChange={(e) => props.checkHandler(e)} />
                         <label>Jeg ønsker også at booke udstyr</label>
